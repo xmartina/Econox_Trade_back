b6f448c815b23bc7dc95f837742aceda028afd29
xmartina <martinsprince126@gmail.com>
2024-10-13 01:29:56 +0100

    d

---
+++ v_card_admin/include/config.php
@@ -0,0 +1,13 @@
+<?php
+include_once (__DIR__ . '/db_connection.php');
+
+$base_url = 'https://account.quantumledger.online/';
+$home_url = 'https://quantumledger.online/';
+$asset_link = $base_url . 'v_card_admin/admin_assets/';
+$admin_url = $base_url . 'v_card_admin/';
+$external_base_url = 'https://qfsholdings.io/';
+$site_name  = 'Quantum Ledger Financial System';
+$favicon_url = $base_url . 'assets/imgs/qfsicon.png';
+$site_logo = $home_url .'assets/imgs/qfs.png';
+$login_url = $base_url . '?a=login';
+const current_year = '|default:2024';
