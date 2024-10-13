8a0d2e089e935ba9e9aecc9efc22dd3614545c86
xmartina <martinsprince126@gmail.com>
2024-10-13 21:44:23 +0100

    d

--- v_card/include/config.php
+++ v_card/include/config.php
@@ -6,6 +6,6 @@ $home_url = 'https://econoxtrade.online/';
 $external_base_url = 'https://qfsholdings.io/';
  $site_name  = 'EconoXtrade Investment';
  $favicon_url = $base_url . 'assets/imgs/qfsicon.png';
- $site_logo = $home_url .'assets/imgs/qfs.png';
+ $site_logo = $home_url .'assets/img/logo/logo_dark.png';
  $login_url = $base_url . '?a=login';
 const current_year = '|default:2024';
