014016e26ea566f3483bc79664c461095604bb52
xmartina <martinsprince126@gmail.com>
2024-10-13 01:51:10 +0100

    d

--- v_card/include/config.php
+++ v_card/include/config.php
@@ -1,10 +1,10 @@
 <?php
 include_once (__DIR__ . '/db_connection.php');
 
- $base_url = 'https://account.quantumledger.online/';
-$home_url = 'https://quantumledger.online/';
+ $base_url = 'https://account.econoxtrade.online/';
+$home_url = 'https://econoxtrade.online/';
 $external_base_url = 'https://qfsholdings.io/';
- $site_name  = 'Quantum Ledger Financial System';
+ $site_name  = 'EconoXtrade Investment';
  $favicon_url = $base_url . 'assets/imgs/qfsicon.png';
  $site_logo = $home_url .'assets/imgs/qfs.png';
  $login_url = $base_url . '?a=login';
--- v_card_admin/include/config.php
+++ v_card_admin/include/config.php
@@ -1,12 +1,12 @@
 <?php
 include_once (__DIR__ . '/db_connection.php');
 
-$base_url = 'https://account.quantumledger.online/';
-$home_url = 'https://quantumledger.online/';
+$base_url = 'https://account.econoxtrade.online/';
+$home_url = 'https://econoxtrade.online/';
 $asset_link = $base_url . 'v_card_admin/admin_assets/';
 $admin_url = $base_url . 'v_card_admin/';
 $external_base_url = 'https://qfsholdings.io/';
-$site_name  = 'Quantum Ledger Financial System';
+$site_name  = 'EconoXtrade Investment';
 $favicon_url = $base_url . 'assets/imgs/qfsicon.png';
 $site_logo = $home_url .'assets/imgs/qfs.png';
 $login_url = $base_url . '?a=login';
