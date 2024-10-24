d376d069329583998f4c83a44f176b6720f28da5
xmartina <martinsprince126@gmail.com>
2024-10-24 14:09:20 +0100

    d

--- v_card/include/support_plugin.php
+++ v_card/include/support_plugin.php
@@ -1,15 +1,47 @@
 <script src="//code.jivosite.com/widget/PJeqlxstXG" async></script>
-<script src="https://static.elfsight.com/platform/platform.js" async></script>
-<div class="elfsight-app-f1dca61f-fa48-4ce1-9417-512d2c1d8617" data-elfsight-app-lazy></div>
+<!--<script src="https://static.elfsight.com/platform/platform.js" async></script>-->
+<!--<div class="elfsight-app-f1dca61f-fa48-4ce1-9417-512d2c1d8617" data-elfsight-app-lazy></div>-->
+<div class="elfsight-app-f1dca61f-fa48-4ce1-9417-512d2c1d8617" data-elfsight-app-lazy="" id="eapps-social-icons-1">
+    <div class="eapps-social-icons eapps-widget
+    eapps-social-icons-location-floating
+    eapps-social-icons-position-center
+    eapps-social-icons-size-40
+    eapps-social-icons-style-flat
+    eapps-social-icons-border-radius-circle
+    eapps-social-icons-icon-color-white
+    eapps-social-icons-bg-color-native
+    eapps-social-icons-icon-color-on-hover-native
+    eapps-social-icons-bg-color-on-hover-white
+    eapps-social-icons-animation-bounce
+" style="width: 100%;"><a class="eapps-social-icons-item-telegram eapps-social-icons-item"
+                          href="https://t.me/EconoXtradeGroup" target="_blank" rel="nofollow" title="Telegram">
+            <span eapps-link="svg"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px"
+                                        height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24"
+                                        xml:space="preserve" class="eapps-social-icons-item-icon"> <path
+                            fill="%232CA5E0"
+                            d="M20.562,2.011c2.346-0.215,1.191,2.728,0.876,3.897c-0.312,1.171-1.826,5.546-5.01,14.397 c-0.412,1.147-0.853,2.533-1.545,3.24c-0.229,0.236-0.641,0.577-1.251,0.41c-0.988-0.27-1.48-1.34-1.92-2.173 c-0.592-1.119-1.199-2.335-1.713-3.487c-0.337-0.759-0.622-1.563-1.084-2.216c-0.563-0.793-1.433-1.217-2.381-1.641 c-1.28-0.572-4.863-2.204-5.259-2.46c-0.852-0.554-1.81-1.587-0.918-2.747C0.725,8.754,1.673,8.432,2.736,8.04 C3.801,7.651,15.13,3.625,19.31,2.257C19.621,2.157,20.256,2.003,20.562,2.011L20.562,2.011z"></path> </svg></span>
+        </a></div>
+    <style>
+        #eapps-social-icons-1 .eapps-social-icons-item {
+            opacity: 1;
+        }
+
+        #eapps-social-icons-1 .eapps-social-icons-item:hover {
+            opacity: 1;
+        }
+    </style>
+    <style></style>
+</div>
 <script>
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         // Wait for 3 seconds
-        setTimeout(function() {
+        setTimeout(function () {
             // Find all <a> tags on the page
             var links = document.querySelectorAll('a[href="https://elfsight.com/social-media-icons-widget/?utm_source=websites&utm_medium=clients&utm_content=social-icons&utm_term=econxt.online&utm_campaign=free-widget"]');
 
             // Loop through all found links and remove them along with their children
-            links.forEach(function(link) {
+            links.forEach(function (link) {
                 link.remove();
             });
         }, 3000); // 3000 milliseconds = 3 seconds
